b8b666de2fc79927163f8bcedddc52526bd85cc4
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-10 01:39:55 -0300

    Add array definition.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/**
+ * An array is a container object that holds a fixed number of values of a single type.
+ * The length of an array is established when the array is created. After creation,
+ * its length is fixed.
+ * Each item in an array is called an element, and each element is accessed by its numerical index.
+ * Numbering begins with 0.
+ * Source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html
+ */
+
 public class Main {
     public static void main(String[] args) {
 
