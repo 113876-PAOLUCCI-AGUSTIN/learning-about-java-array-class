5c2c191f95b215ef8ded22083ac87120c897499f
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-17 13:21:22 -0300

    Update main

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,51 @@
  * SOURCE: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html
  */
 
+/*
+* PROFE DEFINICIONES:
+* (HOMERO) COLECCIONES - ARREGLOS - INTERFACE COLLECTION
+* ARREGLO: tamaño fijo, de ubicar algo en una posición ,desventaja: recorrido.
+* ARREGLO MULTIDIMENSIONAL: TIC TAC TOE.
+* MULTIDIMENSIONAL CUBO RUBIK. EJEMPLO ARREGLO DE ARREGLOS
+*
+*ARRAY LIST: todos los objetos mantienen el orden. Podemos recorrerlo con un indice.
+* Desventaja: es anti performance.
+* Google Keep lista del supermercado.
+*
+* VECTOR: para usar con multiples hilos. Igual que arraylist pero sincronizado. Es seguro para los hilos de ejecución.
+* Ej: carrito, inventario.
+* Race Conditions: Ver diferencia entre hilo simple e hilo multiple.
+*
+* LIST >>>> INTEFAZ
+* CLASES: ARRAY LIST / LINKEDLIST /
+*
+* INTERFAZ QUEUE
+* FIFO /
+* LIFO // PLATOS, NAVEGADOR E HISTORIAL
+*
+* CLASE: LINKEDLIST (NODOS)
+* ES MUY FÁCIL AGREGAR UN ELEMENTO. DESVENTAJA: SACAR UN ELEMENTO EN PARTICULAR.
+* EJEMPLO PARA FILA DE BANCO POR EJEMPLO.
+*
+* INTERFAZ SET: NO DUPLICADOS, NO ELEMENTOS CON INDICES COMO LAS LISTAS.
+*
+* POR QUE MAPA NO ESTA EN COLLECTIONS INTERABLE.
+* 
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* 0*/
+
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
